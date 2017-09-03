55a73b460988472284e4c531ca593cb396cd3ecf
tayyebi <tayyebimohammadreza@gmail.com>
2017-09-03 09:58:03 +0430

    Declaring table variables

--- Main.sql
+++ Main.sql
@@ -1,17 +1,29 @@
 declare @TableName sysname = 'Document'
-declare @Result varchar(max) = 'db.'+@TableName+'.insert('
+declare @Result varchar(max) = 'CREATE @'+@TableName+' TABLE(' +'
+'
 
-select @Result = @Result + ColumnName + ': ' + /* value here */ '' + ', '
+select @Result = @Result + '	' + ColumnName + ' ' + 'nvarchar(300)' + ',
+'
 from
 (
-    select 
-        replace(col.name, ' ', '_') ColumnName
+    select replace(col.name, ' ', '_') ColumnName
     from sys.columns col
-        join sys.types typ on
-            col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
-    where object_id = object_id(@TableName)
+    where object_id = object_id('Document')
+) t
+
+set @Result = @Result  + '	ExceptionHandler char(1)
+'  +');
+INSERT INTO @' + @TableName + '(';
+
+select @Result = @Result + '''' + ColumnName + ' ' + ColumnType + ''','
+from
+(
+    select replace(col.name, ' ', '_') ColumnName, typ.name ColumnType
+    from sys.columns col join sys.types typ on col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
+    where object_id = object_id('Document')
 ) t
 
 set @Result = @Result  + ');'
+
 
 print @Result
