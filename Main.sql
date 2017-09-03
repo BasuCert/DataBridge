874bd90e8757bd8b1323c22f25e21fbc72139d2e
tayyebi <tayyebimohammadreza@gmail.com>
2017-09-03 11:31:23 +0430

    New method

--- Main.sql
+++ Main.sql
@@ -1,40 +1,24 @@
-DECLARE @TableName sysname = 'DocumentGroupSign'
-DECLARE @Result varchar(max) = 'DECLARE @'+@TableName+' TABLE(' +'
-'
-
-select @Result = @Result + '	[' + CONCAT('Column', column_id) + '] ' + 'nvarchar(300)' + ',
-'
-from
-(
-	select col.column_id
-    from sys.columns col
-    where object_id = object_id(@TableName)
-) t
-
-set @Result = @Result  + '	ExceptionHandler char(1)
-'  +');
-INSERT INTO @' + @TableName + ' VALUES (';
-
-select @Result = @Result + '''' + ColumnName + ' ' + ColumnType + ''','
-from
-(
-    select replace(col.name, ' ', '_') ColumnName, typ.name ColumnType
-    from sys.columns col join sys.types typ on col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
-    where object_id = object_id(@TableName)
-) t
-
-set @Result = @Result  + ''''');
-'
+DECLARE @TableName sysname = 'DocumentGroupSign';
+DECLARE @HeadersTable TABLE(ColumnId int,ColumnName nvarchar(300));
 
 
+	INSERT INTO @HeadersTable
+	select column_id, replace(col.name, ' ', '_') ColumnName
+	from sys.columns col join sys.types typ on col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
+	where object_id = object_id(@TableName)
 
-declare @TheSelect nvarchar(max)
+DECLARE @Result varchar(max) = ''
+DECLARE @TheSelect nvarchar(max) = 'SELECT '+'
+	'+ 'REPLACE(' + '''db.'+ @TableName +'.insert( '' + '+'
+	';
+SELECT @TheSelect  = @TheSelect +
+	'CONCAT(''' + ColumnName + ': "'', '+ColumnName+', ''",'') +
+	'
+	from @HeadersTable
 
-set  @TheSelect = 'SELECT * FROM [' + @TableName + '] CROSS JOIN @' + @TableName
-
+SET @TheSelect =@TheSelect +  '
+	' + ''');'', '',);'', '');'')' + '
+AS [Result] FROM [' + @TableName + ']';
 set @Result = @Result  + @TheSelect
-
-
 print @Result
-
 --EXEC (@Result)
