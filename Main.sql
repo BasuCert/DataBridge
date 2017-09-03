418b360866391d305140d283b3cfc8c9c1547943
tayyebi <tayyebimohammadreza@gmail.com>
2017-09-03 10:20:26 +0430

    Column Names

--- Main.sql
+++ Main.sql
@@ -1,14 +1,14 @@
-DECLARE @TableName sysname = 'Document'
+DECLARE @TableName sysname = 'DocumentGroupSign'
 DECLARE @Result varchar(max) = 'DECLARE @'+@TableName+' TABLE(' +'
 '
 
-select @Result = @Result + '	[' + ColumnName + '] ' + 'nvarchar(300)' + ',
+select @Result = @Result + '	[' + CONCAT('Column', column_id) + '] ' + 'nvarchar(300)' + ',
 '
 from
 (
-    select replace(col.name, ' ', '_') ColumnName
+	select col.column_id
     from sys.columns col
-    where object_id = object_id('Document')
+    where object_id = object_id(@TableName)
 ) t
 
 set @Result = @Result  + '	ExceptionHandler char(1)
@@ -20,7 +20,7 @@ from
 (
     select replace(col.name, ' ', '_') ColumnName, typ.name ColumnType
     from sys.columns col join sys.types typ on col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
-    where object_id = object_id('Document')
+    where object_id = object_id(@TableName)
 ) t
 
 set @Result = @Result  + ''''');
@@ -30,11 +30,11 @@ set @Result = @Result  + ''''');
 
 declare @TheSelect nvarchar(max)
 
-set  @TheSelect = 'SELECT * FROM ' + @TableName + ' CROSS JOIN @' + @TableName
+set  @TheSelect = 'SELECT * FROM [' + @TableName + '] CROSS JOIN @' + @TableName
 
 set @Result = @Result  + @TheSelect
 
 
 print @Result
 
-EXEC (@Result)
+--EXEC (@Result)
